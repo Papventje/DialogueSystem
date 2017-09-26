92901bbc704492b6f76165d316c649c67d5d7a82
Papventje <robbierobsnor@gmail.com>
2017-09-26 10:16:07 +0200

    Dialogue System Done

---
+++ Assets/Scripts/CameraLerpWhenTalking.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLerpWhenTalking : MonoBehaviour {
+
+    private float normal = 8f;
+    private float zoomed = 4.5f;
+
+    private float smooth = 5f;
+
+    private bool zoomBool;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (zoomBool)
+        {
+            Debug.Log("zoooom");
+            //Camera.main.orthographicSize = 4.5f;
+            Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, zoomed, Time.deltaTime * smooth);
+        }
+        else
+        {
+            Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, normal, Time.deltaTime * smooth);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "NPC")
+        {
+            zoomBool = true;
+            Debug.Log("hello");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "NPC")
+        {
+            zoomBool = false;
+        }
+    }
+}
---
+++ Assets/Scripts/SetSwordActive.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetSwordActive : MonoBehaviour {
+
+    public Transform sword;
+
+    void Start()
+    {
+        sword.gameObject.SetActive(false);
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Chest" && Input.GetKeyDown(KeyCode.E))
+        {
+            sword.gameObject.SetActive(true);
+        }
+    }
+}
